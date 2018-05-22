78cf2bb9bd49b9b8bc6ede0d7546fc674d22960f
Luc Wollants <luc.wollants@apptite.be>
2018-05-22 08:56:23 +0200

    VSVGVQ-19 Add simple unit test as proof of concept.

---
+++ test/question/category_test.dart
@@ -0,0 +1,20 @@
+import "package:test/test.dart";
+
+import 'package:gvq_admin_ui/src/question/category.dart';
+
+void main() {
+  Category category;
+
+  setUp(() {
+    category = new Category(
+        '1289d4b5-e88e-4b3c-9223-eb2c7c49f4d0', 'EHBO/Ongeval/Verzekering');
+  });
+
+  test('stores an id', () {
+    expect(category.id, '1289d4b5-e88e-4b3c-9223-eb2c7c49f4d0');
+  });
+
+  test('stores a name', () {
+    expect(category.name, 'EHBO/Ongeval/Verzekering');
+  });
+}
