9d035386d404051d2e4f86580339efe01b44775c
Luc Wollants <luc.wollants@apptite.be>
2018-05-25 14:58:46 +0200

    VSVGVQ-30 Make it possible to sort the questions table.

--- lib/src/question/questions_list_component.dart
+++ lib/src/question/questions_list_component.dart
@@ -11,12 +11,30 @@ import 'package:gvq_admin_ui/src/question/question_service.dart';
 
 class QuestionsListComponent implements OnInit {
   List<Question> _questions;
+  List<Question> filteredQuestions;
+  int _idDirection = 1;
+  int _categoryDirection = 1;
 
   @override
   void ngOnInit() async {
     QuestionService questionService = new QuestionService();
     this._questions = await questionService.getAll();
+    this.filteredQuestions = this._questions;
   }
 
-  List<Question> get questions => _questions;
+  void onSortId() {
+    _questions.sort((q1, q2) {
+      return q1.id.compareTo(q2.id) * this._idDirection;
+    });
+    this._idDirection = this._idDirection * -1;
+    this.filteredQuestions = _questions.toList();
+  }
+
+  void onSortCategory() {
+    _questions.sort((q1, q2) {
+      return q1.category.name.compareTo(q2.category.name) * this._categoryDirection;
+    });
+    this._categoryDirection = this._categoryDirection * -1;
+    this.filteredQuestions = _questions.toList();
+  }
 }
